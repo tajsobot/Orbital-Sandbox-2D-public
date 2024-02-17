7a5a49f376e1e20c66bef93c1c56e5cae7ead32f
Taj Šobot <taj.sobot@gmail.com>
2024-02-17 14:49:11 +0100

    DOMA flinging zacetek, "fix" za index out of bounds, nekaj novih screenov

--- desktop/src/com/tajjulo/orbitalsandbox/screens/GameScreen.java
+++ desktop/src/com/tajjulo/orbitalsandbox/screens/GameScreen.java
@@ -359,6 +359,7 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
 			space.addObject(flingingObject);
 			flinging = 2;
 		}
+		//b
 		if(flinging == 2){
 			flingingObject.setPosX(clickPos.x);
 			flingingObject.setPosY(clickPos.y);
